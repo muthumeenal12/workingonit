60ba6a884ed18dac7f451a74faa255806b838e26
muthumeenal12 <muthumeenalv95@gmail.com>
2025-05-15 16:22:11 +0530

    letsdoit

---
+++ config.py
@@ -0,0 +1,26 @@
+import torch
+
+BATCH_SIZE = 16 # Increase / decrease according to GPU memeory.
+RESIZE_TO = 640 # Resize the image for training and transforms.
+NUM_EPOCHS = 75 # Number of epochs to train for.
+NUM_WORKERS = 4 # Number of parallel workers for data loading.
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# Training images and XML files directory.
+TRAIN_DIR = 'data/Train/Train/JPEGImages'
+# Validation images and XML files directory.
+VALID_DIR = 'data/Val/Val/JPEGImages'
+
+# Classes: 0 index is reserved for background.
+CLASSES = [
+    '__background__', 'person'
+]
+
+NUM_CLASSES = len(CLASSES)
+
+# Whether to visualize images after crearing the data loaders.
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# Location to save model and plots.
+OUT_DIR = 'outputs'
